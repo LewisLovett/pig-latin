c6a365c06ce29e91dc974aee10c84da3ad36fb55
Lewis Lovett <llovett7575@gmail.com>
2023-02-15 13:20:58 +0000

    Created pigLatinize method

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -3,7 +3,7 @@ package org.example;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(pigLatinize("pig latin"));
     }
 
     public static String pigLatinize(String phrase){
@@ -11,12 +11,28 @@ public class Solution {
         String pigLatinizedPhrase = "";
         for (String word: wordArray) {
             String pigLatinizedWord = "";
+            int i = 1;
             if(word.charAt(0)=='a' || word.charAt(0)=='e'|| word.charAt(0)=='i'|| word.charAt(0)=='o'|| word.charAt(0)=='u'){
                 pigLatinizedWord += word + "way";
             }else{
+                String consonantCluster = String.valueOf(word.charAt(0));
+                Boolean vowelFound = false;
+
+                while(!vowelFound){
+                    if(word.charAt(i)=='a' || word.charAt(i)=='e'|| word.charAt(i)=='i'|| word.charAt(i)=='o'|| word.charAt(i)=='u'){
+                        vowelFound = true;
+                    }else{
+                        consonantCluster += String.valueOf(word.charAt(i));
+                        i++;
+                    }
+                }
+                pigLatinizedWord = word.substring(i) + '-' + consonantCluster + "ay";
 
             }
             pigLatinizedPhrase += pigLatinizedWord;
+
+            pigLatinizedPhrase+= " ";
         }
+        return  pigLatinizedPhrase;
     }
 }
